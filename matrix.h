ae8ca3df930e8d8b85ad49cae89a4bbbff8d099b
Ramon Wong <rcnwong78@gmail.com>
2023-07-28 20:24:40 -0300

    update matrix header

--- matrix.h
+++ matrix.h
@@ -8,16 +8,16 @@
 #define PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286
 
 
-void	MLoadIdentity(float*);
-void	MFrustum(float *, float, float, float, float, float, float);
-void	LookAtM( float *, float *, float *, float *);
+void	MLoadIdentity(GLfloat*);                                                    // Load the identity matrix
+void	MFrustum(GLfloat *, GLfloat, GLfloat, GLfloat, GLfloat, GLfloat, GLfloat);  // Frustum Matrix, Result, Left, Right, Bottom, Top, Near, Far
+void	LookAtM( GLfloat *, GLfloat *, GLfloat *, GLfloat *);                       // Camera Matrix, Result, position, target, up vector
 
-void	MTranslate(float *, float, float, float);
-void	CrossProduct(float *, float *, float *);
-void	Normalize(float *);
+void	MTranslate(GLfloat *, GLfloat, GLfloat, GLfloat);                           // Translation Matrix, Result, x, y, z
+void	CrossProduct(GLfloat *, GLfloat *, GLfloat *);                              // Cross Product, Result, Vector 1, Vector 2 
+void	Normalize(GLfloat *);                                                       // Normalize, Vector   
 
-void	MMultiply(float *, float *, float *);
-void	MTransform(float *, float *, float *);
+void	MMultiply(GLfloat *, GLfloat *, GLfloat *);                                 // Multiply, Result, Matrix 1, Matrix 2
+void	MTransform(GLfloat *, GLfloat *, GLfloat *);                                // Transform, Result, Matrix, Vector
 
 
 #endif
