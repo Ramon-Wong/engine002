3ff55e423643aaa8e53ae18237871dff02a0c860
Ramon Wong <rcnwong78@gmail.com>
2023-07-31 13:08:12 -0300

    adding functions.h to source

---
+++ functions.h
@@ -0,0 +1,16 @@
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+// Function prototypes
+// uint8_t/int8_t equals to unsigned/signed char
+// uint16_t/int16_t equals to unsigned/signed short
+// uint32_t/int32_t equals to unsigned/signed int
+
+
+
+
+#endif
