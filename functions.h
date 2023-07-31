0b4ecd0786b918ebbd8038c8de8c11337d4c2a56
Ramon Wong <rcnwong78@gmail.com>
2023-07-31 20:21:29 -0300

    updated and added 3d Vectors and matrix array top functions.h

--- functions.h
+++ functions.h
@@ -10,6 +10,21 @@
 // uint16_t/int16_t equals to unsigned/signed short
 // uint32_t/int32_t equals to unsigned/signed int
 
+//I need some interesting sounding names for these types
+typedef int8_t              int8;
+typedef int16_t             int16; 
+typedef int32_t             int32;
+typedef float32_t           float32;    
+
+// 3D vector
+typedef struct {
+    float32_t DATA[3];
+} VECTOR3D;
+
+// Matrix 4x4
+typedef struct {
+    float32_t DATA[16];
+}
 
 
 
