6e117348886a92679f5f28b3b918ac961cf13def
Ramon Wong <rcnwong78@gmail.com>
2023-08-04 10:47:46 -0300

    stash and experiment

--- functions.h
+++ functions.h
@@ -42,19 +42,18 @@ struct VECTOR3D{
         z = Vec1.x * Vec2.y - Vec1.y * Vec2.x;
     }    
 
-    VECTOR3D operator =( const VECTOR3D & v ) const {
-        return VECTOR3D( v.x, v.y, v.z );
-    }
-
-    VECTOR3D operator+( const VECTOR3D & v ) const {
-        return VECTOR3D( x + v.x, y + v.y, z + v.z );
-    }
-
-    VECTOR3D operator-( const VECTOR3D & v ) const {
-        return VECTOR3D( x - v.x, y - v.y, z - v.z );
-    }
+    VECTOR3D operator = ( const VECTOR3D & v ) const {  return VECTOR3D( v.x, v.y, v.z );}
+    VECTOR3D operator + ( const VECTOR3D & v ) const {  return VECTOR3D( x + v.x, y + v.y, z + v.z );}
+    VECTOR3D operator - ( const VECTOR3D & v ) const {  return VECTOR3D( x - v.x, y - v.y, z - v.z );}
 
 };
+
+VECTOR3D        normalize(VECTOR3D);
+
+
+
+
+
 
     // Matrix 4x4
     struct MATRIX4X{
@@ -67,3 +66,26 @@ struct VECTOR3D{
 
     #endif
 
+
+
+
+// void LookAtM( float * Result, float * Pose, float * View, float * UpVx){
+// 	float X[3], Y[3], Z[3];
+// 	Z[0] = View[0] - Pose[0];
+// 	Z[1] = View[1] - Pose[1];
+// 	Z[2] = View[2] - Pose[2];
+// 	Normalize(Z);
+	
+// 	CrossProduct( X, UpVx, Z);      // Compute cross product of UpVx, Z
+// 	Normalize(X);
+	
+// 	CrossProduct( Y, Z, X);         // reCompute vector Y
+// 	Normalize(Y);
+	
+// 	Result[0] = X[0];	Result[4] = X[1];	Result[8] = X[2];
+// 	Result[1] = Y[0];	Result[5] = Y[1];	Result[9] = Y[2];
+// 	Result[2] = Z[0];	Result[6] = Z[1];	Result[10] = Z[2];
+// 	Result[3] = 0.0;	Result[7] = 0.0;	Result[11] = 0.0;
+	
+// 	MTranslate( Result, -View[0], -View[1], -View[2]);
+// }
