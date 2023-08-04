b5b31149f766f930e64bd21c5d3b44cf17b4ec55
Ramon Wong <rcnwong78@gmail.com>
2023-08-03 21:30:55 -0300

    stash

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include "functions.h"
+#include "matrix.h"
 
 
 
 int main(int argc, char *argv[]){
+
+    float Mat[16] = {   1.0f, 0.0f, 0.0f, 0.0f,
+                        0.0f, 1.0f, 0.0f, 0.0f,
+                        0.0f, 0.0f, 1.0f, 0.0f,
+                        0.0f, 0.0f, 0.0f, 1.0f};
+
+    float Pose[3] = {0.0f, 0.0f, 0.0f};
+    float View[3] = {0.0f, 0.0f, 1.0f};
+    float UpVx[3] = {0.0f, 1.0f, 0.0f};
+
+
+    LookAtM( Mat, Pose, View, UpVx);
+
 
     printf("Hello World!\n");
 
