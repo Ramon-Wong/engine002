32820844d1b95e545d5407e7c4a42e545e5f7746
Ramon Wong <rcnwong78@gmail.com>
2023-08-03 21:46:47 -0300

    moved the functions.h from the matrix branch

---
+++ functions.h
@@ -0,0 +1,29 @@
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <assert.h>
+#include <string.h>
+#include <math.h>
+
+// Function prototypes
+// uint8_t/int8_t equals to unsigned/signed char
+// uint16_t/int16_t equals to unsigned/signed short
+// uint32_t/int32_t equals to unsigned/signed int
+
+//I need some interesting sounding names for these types
+typedef int8_t              int8;
+typedef int16_t             int16; 
+typedef int32_t             int32;
+
+// 3D vector
+typedef struct {            float DATA[3];}     VECTOR3D;
+
+// Matrix 4x4
+typedef struct {            float DATA[16];}    MATRIX4X;
+
+
+#endif
