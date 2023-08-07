0c51e3c193823f6ac9560a41eafc313d1ab33f9b
Ramon Wong <rcnwong78@gmail.com>
2023-08-07 12:32:38 -0300

    added functions.h into project

---
+++ functions.h
@@ -0,0 +1,30 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <GL/glew.h>
+#include <GL/glfw.h>
+#include "Matrix4x.h"
+
+
+extern GLuint		GLSL_Program;
+extern GLuint		GLSL_vertex;
+extern GLuint		GLSL_fragment;
+
+extern GLfloat		Proj_Matrix[16];
+extern GLfloat		View_Matrix[16];
+
+
+void				Init(void);
+void				Shut_Down(int);
+void				Main_Loop(void);
+void				Draw_Square(void);
+void				Draw(void);
+
+char *				ReadFile(const char *);
+void				ShaderSetup();
+
+#endif
