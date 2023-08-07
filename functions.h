ddff2c253e87acf9b62f81d4e79cf7a199879e2c
Ramon Wong <rcnwong78@gmail.com>
2023-08-07 13:38:44 -0300

    updated functions with the LookAtM in the header

--- functions.h
+++ functions.h
@@ -41,8 +41,9 @@ struct VECTOR3D{
 
 VECTOR3D        normalize(VECTOR3D);
 VECTOR3D        crossproduct(VECTOR3D, VECTOR3D);
+float           dotproduct(VECTOR3D, VECTOR3D);
 
-
+void            LookAtM( MATRIX4X *, VECTOR3D *, VECTOR3D *, VECTOR3D *);
 
 // Matrix 4x4
 struct MATRIX4X{
@@ -57,24 +58,3 @@ struct MATRIX4X{
 
 
 
-
-// void LookAtM( float * Result, float * Pose, float * View, float * UpVx){
-// 	float X[3], Y[3], Z[3];
-// 	Z[0] = View[0] - Pose[0];
-// 	Z[1] = View[1] - Pose[1];
-// 	Z[2] = View[2] - Pose[2];
-// 	Normalize(Z);
-	
-// 	CrossProduct( X, UpVx, Z);      // Compute cross product of UpVx, Z
-// 	Normalize(X);
-	
-// 	CrossProduct( Y, Z, X);         // reCompute vector Y
-// 	Normalize(Y);
-	
-// 	Result[0] = X[0];	Result[4] = X[1];	Result[8] = X[2];
-// 	Result[1] = Y[0];	Result[5] = Y[1];	Result[9] = Y[2];
-// 	Result[2] = Z[0];	Result[6] = Z[1];	Result[10] = Z[2];
-// 	Result[3] = 0.0;	Result[7] = 0.0;	Result[11] = 0.0;
-	
-// 	MTranslate( Result, -View[0], -View[1], -View[2]);
-// }
