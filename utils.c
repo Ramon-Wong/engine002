4ca8a129b82df430b21a022441864124a2466b11
Ramon Wong <rcnwong78@gmail.com>
2023-08-08 09:37:04 -0300

    it runs

--- utils.c
+++ utils.c
@@ -5,25 +5,27 @@ void Init(void){
     const int window_width = 800;
     const int window_height = 600;
  
-    if( glfwInit() != GL_TRUE)
-    Shutdown(1);
-  
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
   	GLFWwindow * window = glfwCreateWindow( window_width, window_height, "LearnOpenGL", NULL, NULL);
 	if( !window ){
 		Shutdown(1);
 	}
-		
+
+ 	glfwMakeContextCurrent(window);		
  
-	  printf("GL VENDOR:--- %s \n", glGetString(GL_VENDOR));
-	  printf("GL RENDERER:- %s \n", glGetString(GL_RENDERER));
-	  printf("GL VERSION:-- %s \n", glGetString(GL_VERSION));
-	  printf("GL SHADING:-- %s \n", glGetString(GL_SHADING_LANGUAGE_VERSION));
- 
+	printf("GL VENDOR:--- %s \n", glGetString(GL_VENDOR));
+	printf("GL RENDERER:- %s \n", glGetString(GL_RENDERER));
+	printf("GL VERSION:-- %s \n", glGetString(GL_VERSION));
+	printf("GL SHADING:-- %s \n", glGetString(GL_SHADING_LANGUAGE_VERSION));
  }
 
 
 void Shutdown(int return_code){
-/	
+
 	glfwTerminate();
 	exit(return_code);
 }
