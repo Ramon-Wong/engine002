a0f6b14dc33ac56291906989aa04f12bb7b58c51
Ramon Wong <rcnwong78@gmail.com>
2023-08-08 17:22:47 -0300

    added ReadFile

--- shaderutils.c
+++ shaderutils.c
@@ -83,3 +83,40 @@ void	print_program_info_log(GLuint programme) {
 	glGetProgramInfoLog (programme, max_length, &actual_length, log);
 	printf ("program info log for GL index %u:\n%s", programme, log);
 }
+
+
+
+
+char *	ReadFile(const char * path){
+	
+	//~ usage										<==
+	//~ 											<==
+	//~ char * txt;									<==
+	//~ 											<==	
+	//~ txt = ReadFile("GLSL/loremipsum.txt");		<==
+	//~ printf(" txt: %s", txt);					<==
+	//~ free(txt);									<== memory leak
+	//~ 											<==
+	
+	FILE *	tFile;
+	char *	buffer;
+	int		lSize;
+	
+	tFile = fopen( path, "rt");
+	if(tFile){
+		
+		fseek( tFile, 0, SEEK_END);
+		lSize = ftell( tFile);
+		rewind( tFile);
+		
+		buffer = (char*) malloc( sizeof(char)*(lSize+1));
+		fread( buffer, 1, lSize, tFile);
+		buffer[lSize] = '\0';
+		
+		fclose(tFile);
+	}else{
+		printf("Error at opening file: %s", path);
+	}
+	
+	return buffer;
+}
