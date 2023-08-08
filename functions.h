4ac3bd4d6fb4c99de9a1f403698e63b595e9e6c8
Ramon Wong <rcnwong78@gmail.com>
2023-08-07 22:14:17 -0300

    stash

---
+++ functions.h
@@ -0,0 +1,21 @@
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <assert.h>
+#include <string.h>
+#include <math.h>
+
+#include <GL/glew.h>
+#include <GL/glfw.h>
+#include <GL/glu.h>
+
+
+
+
+
+
+#endif
