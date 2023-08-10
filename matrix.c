1e5a5a390037b932206fff7dbc2d2018a0531277
Ramon Wong <rcnwong78@gmail.com>
2023-08-10 18:58:50 -0300

    updated version. LookAtM with dotproduct calculation

--- matrix.c
+++ matrix.c
@@ -49,12 +49,12 @@ void LookAtM( float * Mat, float * Pose, float * View, float * UpVx){
 	CrossProduct( Y, Z, X); // reCompute vector Y
 	Normalize(Y);
 	
-	Mat[0] = X[0];	Mat[4] = X[1];	Mat[8] = X[2];
-	Mat[1] = Y[0];	Mat[5] = Y[1];	Mat[9] = Y[2];
-	Mat[2] = Z[0];	Mat[6] = Z[1];	Mat[10] = Z[2];
-	Mat[3] = 0.0;	Mat[7] = 0.0;	Mat[11] = 0.0;
+	Mat[0]	= X[0];	Mat[4]	= X[1];	Mat[8]	= X[2];	Mat[12]	= -dotproduct( X, View );
+	Mat[1]	= Y[0];	Mat[5]	= Y[1];	Mat[9]	= Y[2]; Mat[13]	= -dotproduct( Y, View );
+	Mat[2]	= Z[0];	Mat[6]	= Z[1];	Mat[10] = Z[2]; Mat[14]	= -dotproduct( Z, View );
+	Mat[3]	= 0.0;	Mat[7]	= 0.0;	Mat[11] = 0.0;	Mat[15]	= 1.0;
 	
-	MTranslate( Mat, -View[0], -View[1], -View[2]);
+	// MTranslate( Mat, -View[0], -View[1], -View[2]);
 }
 
 
@@ -105,6 +105,6 @@ void MTransform(float * Result, float * Data, float * Vec){
 }
 
 
-GLfloat dotproduct(GLfloat * V1, GLfloat * V2){
+float dotproduct( float * V1, float * V2){
 	return V1[0] * V2[0] + V1[1] * V2[1] + V1[2] * V2[2];
 }
