5e2efc4315abdb9b06f583b688f31ecb1f9539c2
Ramon Wong <rcnwong78@gmail.com>
2023-08-11 16:04:38 -0300

    not working version

--- main.c
+++ main.c
@@ -14,24 +14,14 @@
 
 int main(int argc, char *argv[]){
 
-    // unsigned int		GLSL_Program;
-    // unsigned int		GLSL_vertex;
-    // unsigned int		GLSL_fragment;
-
-    // GLfloat				Proj_Matrix[16];
-    // GLfloat				View_Matrix[16];
-    // GLfloat				View_Proj[16];
-
-    // GLuint				uMatLoc[5];
-
-    float points[] = {
+    GLfloat points[] = {
         0.0f,  0.5f,  0.0f,
         0.5f, -0.5f,  0.0f,
        -0.5f, -0.5f,  0.0f
     };
 
-    GLuint indices[] = {
-        0, 1, 2
+    GLubyte indices[] = {
+        0, 2, 1
     };    
 
 	const int window_width  = 800;
@@ -41,24 +31,6 @@ int main(int argc, char *argv[]){
     printf("Hello World!\n");
     Init( window_width, window_height, "prototype GLFW window");
 	ShaderSetup("vshader.glsl", "fshader.glsl", &GLSL_Program, GLSL_vertex, GLSL_fragment);
-
-
-	GLuint vbo = 0;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), &points, GL_STATIC_DRAW);
-
-	GLuint vao = 0;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-
-    GLuint ebo = 0;
-    glGenBuffers(1, &ebo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 
 	MLoadIdentity(View_Matrix);
@@ -88,10 +60,18 @@ int main(int argc, char *argv[]){
 			Shutdown(0);
 		}
 
-		glUseProgram(GLSL_Program);
-		glBindVertexArray(vao);
-         glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
-		// glDrawArrays(GL_TRIANGLES, 0, 3);
+		// old way of drawing a triangle using system memory instead of VBO
+		// this method sucks and is slow, but it works and is easy to understand
+
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glVertexPointer(3, GL_FLOAT, 0, points);
+
+		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_BYTE, indices);
+	
+		glDisableClientState(GL_VERTEX_ARRAY);
+
+
+
 
 		glfwPollEvents();
 		glfwSwapBuffers(glfwGetCurrentContext());
