80212577da1784da9659f4bbcec4b8da2d01dd70
Ramon Wong <rcnwong78@gmail.com>
2023-08-12 11:54:57 -0300

    clean up and stash

--- main.c
+++ main.c
@@ -35,12 +35,11 @@ int main(void){
     glfwGetFramebufferSize(window, &width, &height);
     ratio = (float) width / (float) height;
 
+    glViewport(0, 0, width, height);
 
 
   while (!glfwWindowShouldClose(window)){
 
-
-    glViewport(0, 0, width, height);
     glClear(GL_COLOR_BUFFER_BIT);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -51,14 +50,6 @@ int main(void){
 
     Draw_Square();
 
-    // glBegin(GL_TRIANGLES);
-    // glColor3f(1.f, 0.f, 0.f);
-    // glVertex3f(-0.6f, -0.4f, 0.f);
-    // glColor3f(0.f, 1.f, 0.f);
-    // glVertex3f(0.6f, -0.4f, 0.f);
-    // glColor3f(0.f, 0.f, 1.f);
-    // glVertex3f(0.f, 0.6f, 0.f);
-    // glEnd();
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
@@ -66,7 +57,6 @@ int main(void){
   glfwTerminate();
   exit(EXIT_SUCCESS);
 }
-
 
 
 
