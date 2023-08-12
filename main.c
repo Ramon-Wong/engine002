d6ba6facd816f6c54c13795e9516a8c3b36e831c
Ramon Wong <rcnwong78@gmail.com>
2023-08-11 21:49:16 -0300

    the broken version

--- main.c
+++ main.c
@@ -48,12 +48,12 @@ int main(int argc, char *argv[]){
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), &points, GL_STATIC_DRAW);
 
-	GLuint vao = 0;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	// GLuint vao = 0;
+	// glGenVertexArrays(1, &vao);
+	// glBindVertexArray(vao);
+	// glEnableVertexAttribArray(0);
+	// glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	// glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
     GLuint ebo = 0;
     glGenBuffers(1, &ebo);
