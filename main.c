349886f54e764ec91e994fce4900f010501929d8
Ramon Wong <rcnwong78@gmail.com>
2023-08-20 22:14:21 -0300

    working version

--- main.c
+++ main.c
@@ -1,64 +1,77 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <GL/glu.h>
 #include <stdlib.h>
 #include <stdio.h>
-
-	GLfloat vertices[] = {	-1.0,  1.0, 0.0,		// 0
-							            -1.0, -1.0, 0.0,		// 1
-							             1.0, -1.0, 0.0,		// 2
-							             1.0,  1.0, 0.0};		// 3
-
-	GLubyte indices[] = { 0, 1, 2, 0, 2, 3};
+#include <stdint.h>
 
 
-// GLfloat vertices[]	= {  0.5f, 0.5f, 12.0f, 
-//             						-0.5f, 0.5f, 12.0f,
-//             						-0.5f,-0.5f, 12.0f, 
-// 						             0.5f,-0.5f, 12.0f};	
 
-// GLubyte indices[]	= {  0, 1, 2, 2, 3, 0}; 
+void SizeOpenGLScreen(int width, int height){
+	if(height==0){
+		height=1;
+	}
 
+	glViewport( 0, 0, width, height);
 
-void Draw_Square();
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective( 45.0f, (GLfloat)width/(GLfloat)height, .5f ,150.0f);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
 
 int main(void){
-  GLFWwindow * window;
 
-  if (!glfwInit())
-    exit(EXIT_FAILURE);
-  window = glfwCreateWindow( 800, 600, "Chapter 1: Simple GLFW Example", NULL, NULL);
+    GLFWwindow      * window;
+	  const GLubyte		*	renderer;
+	  const GLubyte		*	version;
 
-  if (!window){
-    glfwTerminate();
-    exit(EXIT_FAILURE);
-  }
+    GLfloat vertices[] = {	 -1.0,  1.0, 0.0,		// 0
+							               -1.0, -1.0, 0.0,		// 1
+							                1.0, -1.0, 0.0,		// 2
+							                1.0,  1.0, 0.0};		// 3
 
-  glfwMakeContextCurrent(window);
+	  GLubyte indices[] = { 0, 1, 2, 0, 2, 3};
 
     // float ratio;
-    int width, height;
+    int width   = 800;
+    int height  = 600;
+
+    glfwInit();
+    window = glfwCreateWindow( width, height, "Simple GLFW Example", NULL, NULL);
+
+    if (!window){
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+	  glewExperimental = GL_TRUE;
+	  // glewInit();
+ 
+	  renderer	= glGetString(GL_RENDERER);
+	  version		= glGetString(GL_VERSION);
+	
+	  printf("\n Renderer: %s", renderer);
+	  printf("\n OpenGL version supported %s", version);
+	  printf("\n GL VENDOR:---  %s ",	glGetString(GL_VENDOR));
+	  printf("\n GL VERSION:--  %s ",  	glGetString(GL_VERSION));
+	  printf("\n GL SHADING:--  %s ",	glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+	  glEnable( GL_DEPTH_TEST);
+	  glDepthFunc( GL_LESS);
+
+
 
     glfwGetFramebufferSize(window, &width, &height);
     // ratio = (float) width / (float) height;
 
-    glViewport(0, 0, width, height);
+    SizeOpenGLScreen( width, height);
 
   while (!glfwWindowShouldClose(window)){
-
-    // glClear(GL_COLOR_BUFFER_BIT);
-    // glMatrixMode(GL_PROJECTION);
-    // glLoadIdentity();
-
-    // gluLookAt( 0.0, 0.0, 6.0, 0.0, 0.0, 12.0, 0.0, 1.0, 0.0);
-    // glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-    // glMatrixMode(GL_MODELVIEW);
-    // glLoadIdentity();
-    // glRotatef((float)glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-
-    // Draw_Square();
-
-    // glfwSwapBuffers(window);
-    // glfwPollEvents();
 
 		if(glfwGetKey( window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
 			glfwSetWindowShouldClose( window, 1);
@@ -88,11 +101,3 @@ int main(void){
 
 
 
-void Draw_Square(){
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glVertexPointer(3, GL_FLOAT, 0, vertices);
-	
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, indices);
-	
-	glDisableClientState(GL_VERTEX_ARRAY); // disable vertex arrays
-}
