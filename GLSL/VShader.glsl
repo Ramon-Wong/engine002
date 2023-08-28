a373f1cd955d05283738102525e3779dfd906f84
Ramon Wong <rcnwong78@gmail.com>
2023-08-28 10:39:06 -0300

    update shader to 330 or downgrade

--- GLSL/VShader.glsl
+++ GLSL/VShader.glsl
@@ -1,16 +1,13 @@
-#version 400
+#version 330
 
 uniform mat4        uProjView;
-uniform float       PI;
-uniform float       rotate_z;
 
-in vec3 inPosition;  // Input vertex position
-in vec2 inTexCoord;
+in vec3             position;
+in vec2             textureCoordinates;
 
-out vec2 texCoord;
+out vec2            vTextureCoordinates;
 
 void main() {
-
-    gl_Position     = uProjView * vec4( inPosition, 1.0);
-    texCoord        = inTexCoord;
+    gl_Position         = uProjView * vec4(position, 1.0);
+    vTextureCoordinates = textureCoordinates;
 }
