03a5bf00f64a781e0962e2ced486ca9b4613d8d0
Ramon Wong <rcnwong78@gmail.com>
2023-08-28 12:05:03 -0300

    cleaning

--- functions.c
+++ functions.c
@@ -62,17 +62,6 @@ void Main_Loop(void){
 		printf("\nCan't open tga file");
     } else {
 
-		// glGenTextures(1, &m_texture);
-		// glBindTexture(GL_TEXTURE_2D, m_texture);
-
-		// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
-		// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-		// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-		// glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, x, y, 0, GL_RGB, GL_UNSIGNED_BYTE, data); 
- 		// glGenerateMipmap(GL_TEXTURE_2D);
-		// glBindTexture(GL_TEXTURE_2D, 0);
 		glTexture( &m_texture, data, x, y, n);
 
 
