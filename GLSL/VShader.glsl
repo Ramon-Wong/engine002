61ca4ff9df7af37cdab49c992244e9a5554d782a
Ramon Wong <rcnwong78@gmail.com>
2023-08-30 15:21:39 -0300

    clean up

--- GLSL/VShader.glsl
+++ GLSL/VShader.glsl
@@ -1,22 +1,13 @@
 #version 400
 
 uniform mat4 uProjView;
-uniform float PI;
-uniform float rotate_z;
 
 in vec3 inPosition;  // Input vertex position
-
 out vec4 fragColor;  // Output fragment color (you can modify this based on your needs)
 
 void main() {
     vec3 a = inPosition;
-    vec3 b = a;
 
-    float rad_angle = rotate_z * PI / 180.0;
-
-    b.x = a.x * cos(rad_angle) + a.y * sin(rad_angle);
-    b.y = a.y * cos(rad_angle) - a.x * sin(rad_angle);
-
-    gl_Position = uProjView * vec4(b, 1.0);
+    gl_Position = uProjView * vec4(a, 1.0);
     fragColor = vec4(1.0, 1.0, 1.0, 1.0);  // Example: setting the fragment color to white
 }
--- functions.c
+++ functions.c
@@ -59,16 +59,7 @@ void Main_Loop(void){
  
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
-		glUseProgram( GLSL_Program);
-		
-		uMatLoc[0]	= glGetUniformLocation( GLSL_Program, "RED");
-		glUniform1f( uMatLoc[0], 1.0f);
-		
-		uMatLoc[1]	= glGetUniformLocation( GLSL_Program, "PI");
-		glUniform1f( uMatLoc[1], PI);
-				
-		uMatLoc[2]	= glGetUniformLocation( GLSL_Program, "rotate_z");
-		glUniform1f( uMatLoc[2], rotate_z);		
+		glUseProgram( GLSL_Program);		
 
 		uMatLoc[3]	= glGetUniformLocation( GLSL_Program, "uProjView");
 		glUniformMatrix4fv( uMatLoc[3], 1, GL_FALSE, ProjView);		
