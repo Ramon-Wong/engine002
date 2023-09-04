6b94b3015fd39c44ff45513a4f725195eeabb8be
Ramon Wong <rcnwong78@gmail.com>
2023-09-04 20:03:58 -0300

    stash

--- GLSL/FShader.glsl
+++ GLSL/FShader.glsl
@@ -1,6 +1,5 @@
 #version 400
 
-in vec4             fragColor;              // Input fragment color from the vertex shader
 in vec2             vTexCoord;              // texcoords out
 
 out vec4            fColor;            // Output fragment color
--- GLSL/VShader.glsl
+++ GLSL/VShader.glsl
@@ -6,7 +6,6 @@ in vec3 inPosition;     // Input vertex position
 in vec3 aColor;         // Input Colors
 in vec2 texCoord;       // texcoords
 
-out vec4 fragColor;     // Output fragment color (you can modify this based on your needs)
 out vec2 vTexCoord;     // texcoords out
 
 
@@ -15,5 +14,4 @@ void main() {
     vTexCoord   = texCoord;
 
     gl_Position = uProjView * vec4(a, 1.0);
-    fragColor   = vec4( aColor.xyz, 1.0);  // Example: setting the fragment color to white
 }
