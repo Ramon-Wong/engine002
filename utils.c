b54edd7bce6315d18e0a0acef3cb490b14298247
Ramon Wong <rcnwong78@gmail.com>
2023-09-15 11:07:04 -0300

    added LoadTexture but forgot how to use CreateTexture

--- utils.c
+++ utils.c
@@ -31,6 +31,7 @@ void Init(void){
 
  }
 
+
 void Shut_Down(int return_code){
 		
 	if( GLSL_Program){
@@ -129,6 +130,47 @@ void CreateTexture( GLenum tTarget, GLuint * texture, unsigned char * data, int 
 }
 
 
+
+GLuint LoadTexture(const char * path, const char * tagname, int location){
+
+    int x,y,n;
+	printf("\nLoading %s", path);
+    unsigned char * data = stbi_load( path, &x, &y, &n, 0);
+
+	if (data == NULL) { 
+		printf("\nCan't open tga file");
+		return 0;
+    } else {
+		GLuint			texture;
+
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+		if(n == 3) 
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, x, y, 0,  GL_RGB,	GL_UNSIGNED_BYTE, data);
+		else if (n == 4) 
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, x, y, 0, GL_RGBA,	GL_UNSIGNED_BYTE, data);
+		// glGenerateMipmap(GL_TEXTURE_2D);
+
+		glUseProgram( GLSL_Program);                                                // Use the shader program
+		GLuint textureLocation = glGetUniformLocation(  GLSL_Program, tagname);
+		
+		glBindTexture(GL_TEXTURE_2D, texture);                                    	// Bind your texture to GL_TEXTURE0    
+		glUniform1i(textureLocation, location);                                     // 0 corresponds to GL_TEXTURE0
+
+		stbi_image_free(data);
+		glBindTexture(GL_TEXTURE_2D, 0);
+		printf("\n texture Process %i/%i/%i \n", x, y, n);
+
+		return texture;
+    }
+}
 
 
 
