b642729a347305d7cf016af4b7382d12e7e7f058
Ramon Wong <rcnwong78@gmail.com>
2023-09-15 10:47:04 -0300

    cleaning functions.h/utils.c

---
+++ functions.h
@@ -0,0 +1,37 @@
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "matrix.h"
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <GL/glu.h> 
+#include <math.h>
+
+#include "matrix.h"
+
+
+extern GLuint		GLSL_Program;
+extern GLuint		GLSL_Fbo;
+extern GLuint		GLSL_vertex;
+extern GLuint		GLSL_fragment;
+extern GLFWwindow * wnd;
+
+
+extern GLfloat		Proj_Matrix[16];
+extern GLfloat		View_Matrix[16];
+
+
+void				Init(void);
+void				Shut_Down(int);
+void				Main_Loop(void);
+
+void				ShaderSetup(const char *, const char *);
+void                CreateTexture( GLenum, GLuint *, unsigned char *, int, int, GLenum);
+
+void                SetupVAO( GLuint *, GLuint *, GLuint *, GLfloat *, GLfloat *, GLubyte *, GLsizei, GLsizei, GLsizei);
+void                SetupVAOArray( GLuint *, GLuint *, GLuint *, GLfloat *, GLfloat *, GLfloat *, GLubyte *, GLsizei, GLsizei, GLsizei, GLsizei);
+
+#endif
---
+++ utils.c
@@ -0,0 +1,116 @@
+#include "functions.h"
+
+
+
+void Init(void){
+ 
+	if( glfwInit() != GL_TRUE)
+	Shut_Down(1);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	wnd = glfwCreateWindow( 800, 600, "Hello Triangle", NULL, NULL);
+
+	if(!wnd) {
+		fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+		glfwTerminate();
+	
+	}
+
+	glfwMakeContextCurrent(wnd);
+ 
+	printf("GL VENDOR:--- %s \n", glGetString(GL_VENDOR));
+	printf("GL RENDERER:- %s \n", glGetString(GL_RENDERER));
+	printf("GL VERSION:-- %s \n", glGetString(GL_VERSION));
+	printf("GL SHADING:-- %s \n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+ 
+	GLSL_Program		= 0;
+	GLSL_fragment		= 0;
+	GLSL_vertex			= 0;	
+
+ }
+
+void Shut_Down(int return_code){
+		
+	if( GLSL_Program){
+		glDeleteProgram( GLSL_Program);
+		glDeleteShader( GLSL_vertex);
+		glDeleteShader( GLSL_fragment);		
+	}
+
+	glfwTerminate();
+	exit(return_code);
+}
+
+
+void SetupVAO(GLuint * vao, GLuint * vbo, GLuint * ebo, GLfloat * vertices, GLfloat * colors, GLubyte * indices, GLsizei vsize, GLsizei csize, GLsizei isize){
+    glGenVertexArrays(1, vao);
+    glBindVertexArray(*vao);
+
+    glGenBuffers(1, vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, *vbo);
+    
+    glBufferData(GL_ARRAY_BUFFER, vsize + csize, NULL, GL_STATIC_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, vsize, vertices);
+    glBufferSubData(GL_ARRAY_BUFFER, vsize, csize, colors);
+
+    glGenBuffers(1, ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, isize, indices, GL_STATIC_DRAW);
+
+    // Set up vertex attribute pointers for position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Set up vertex attribute pointers for color
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)(intptr_t)vsize);
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+
+void SetupVAOArray( GLuint * vao, GLuint * vbo, GLuint * ebo, 
+                    GLfloat * array1, GLfloat * array2, GLfloat * array3, 
+                    GLubyte * indices, GLsizei size0,
+                    GLsizei size1, GLsizei size2, GLsizei size3){
+
+    glGenVertexArrays(1, vao);
+    glBindVertexArray(*vao);
+
+    glGenBuffers(1, vbo);
+
+    glBindBuffer(GL_ARRAY_BUFFER, *vbo); 
+    glBufferData(GL_ARRAY_BUFFER, size1 + size2 + size3, NULL, GL_STATIC_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER,     0,             size1, array1);
+    glBufferSubData(GL_ARRAY_BUFFER, size1,             size2, array2);
+    glBufferSubData(GL_ARRAY_BUFFER, size1 + size2,     size3, array3);
+
+    glGenBuffers(1, ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, size0, indices, GL_STATIC_DRAW);
+
+    // Set up vertex attribute pointers for array1
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)(intptr_t) 0);
+    glEnableVertexAttribArray(0);
+
+    // Set up vertex attribute pointers for array2
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)(intptr_t) size1);
+    glEnableVertexAttribArray(1);
+
+    // Set up vertex attribute pointers for array3
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), (void*)(intptr_t) size1 + size2);
+    glEnableVertexAttribArray(2);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+
+
+
+
+
+
