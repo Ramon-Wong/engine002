b36d1d58787cc9aac9ccaad9feac5fe2a6788003
Ramon Wong <rcnwong78@gmail.com>
2023-09-21 22:02:06 -0300

    stash function for projxmodel view

--- functions.c
+++ functions.c
@@ -6,6 +6,7 @@
 
 
 void Main_Loop(){
+    int i = 0;
 
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
@@ -31,6 +32,43 @@ void Main_Loop(){
                   0.0, 0.0, 0.0, // Look at position (center)
                   0.0, 1.0, 0.0); // Up direction
         // Implement your rendering code here, including any objects or transformations
+
+        if( i == 0){
+	        float   proj[16];								// This will hold our projection matrix
+	        float   modl[16];								// This will hold our modelview matrix
+	        float   clip[16];		            
+
+	        glGetFloatv( GL_PROJECTION_MATRIX, proj );
+	        glGetFloatv( GL_MODELVIEW_MATRIX, modl );
+
+	        clip[ 0] = modl[ 0] * proj[ 0] + modl[ 1] * proj[ 4] + modl[ 2] * proj[ 8] + modl[ 3] * proj[12];
+	        clip[ 1] = modl[ 0] * proj[ 1] + modl[ 1] * proj[ 5] + modl[ 2] * proj[ 9] + modl[ 3] * proj[13];
+	        clip[ 2] = modl[ 0] * proj[ 2] + modl[ 1] * proj[ 6] + modl[ 2] * proj[10] + modl[ 3] * proj[14];
+	        clip[ 3] = modl[ 0] * proj[ 3] + modl[ 1] * proj[ 7] + modl[ 2] * proj[11] + modl[ 3] * proj[15];
+
+	        clip[ 4] = modl[ 4] * proj[ 0] + modl[ 5] * proj[ 4] + modl[ 6] * proj[ 8] + modl[ 7] * proj[12];
+	        clip[ 5] = modl[ 4] * proj[ 1] + modl[ 5] * proj[ 5] + modl[ 6] * proj[ 9] + modl[ 7] * proj[13];
+	        clip[ 6] = modl[ 4] * proj[ 2] + modl[ 5] * proj[ 6] + modl[ 6] * proj[10] + modl[ 7] * proj[14];
+	        clip[ 7] = modl[ 4] * proj[ 3] + modl[ 5] * proj[ 7] + modl[ 6] * proj[11] + modl[ 7] * proj[15];
+
+	        clip[ 8] = modl[ 8] * proj[ 0] + modl[ 9] * proj[ 4] + modl[10] * proj[ 8] + modl[11] * proj[12];
+	        clip[ 9] = modl[ 8] * proj[ 1] + modl[ 9] * proj[ 5] + modl[10] * proj[ 9] + modl[11] * proj[13];
+	        clip[10] = modl[ 8] * proj[ 2] + modl[ 9] * proj[ 6] + modl[10] * proj[10] + modl[11] * proj[14];
+	        clip[11] = modl[ 8] * proj[ 3] + modl[ 9] * proj[ 7] + modl[10] * proj[11] + modl[11] * proj[15];
+
+	        clip[12] = modl[12] * proj[ 0] + modl[13] * proj[ 4] + modl[14] * proj[ 8] + modl[15] * proj[12];
+	        clip[13] = modl[12] * proj[ 1] + modl[13] * proj[ 5] + modl[14] * proj[ 9] + modl[15] * proj[13];
+	        clip[14] = modl[12] * proj[ 2] + modl[13] * proj[ 6] + modl[14] * proj[10] + modl[15] * proj[14];
+	        clip[15] = modl[12] * proj[ 3] + modl[13] * proj[ 7] + modl[14] * proj[11] + modl[15] * proj[15];
+
+
+            for(int n = 0; n < 4; n++){
+                printf("\n clip: %f//%f//%f//%f", clip[0+n], clip[1+n], clip[2+n], clip[3+n]);
+            }    
+
+            i = 1;
+        }
+
         glBegin(GL_QUADS);
         glVertex3f(-0.5f,-0.5f, 0.0f);
         glVertex3f( 0.5f,-0.5f, 0.0f);
