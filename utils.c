bd5dbd497679ef48a77a8bd7cf4f2ea9443cb407
Ramon Wong <rcnwong78@gmail.com>
2023-09-28 11:21:35 -0300

    added utils.c and SetupVAOArray in the version

---
+++ utils.c
@@ -0,0 +1,39 @@
+#include "functions.h"
+
+
+
+void SetupVAOArray( GLuint * vao, GLuint * vbo, GLuint * ebo, 
+                    GLfloat * array1, GLfloat * array2, GLfloat * array3, 
+                    GLubyte * indices, GLsizei size0,
+                    GLsizei size1, GLsizei size2, GLsizei size3){
+
+    glGenVertexArrays(1, vao);
+    glBindVertexArray(*vao);
+
+    glGenBuffers(1, vbo);
+
+    glBindBuffer(GL_ARRAY_BUFFER, *vbo); 
+    glBufferData(GL_ARRAY_BUFFER, size1 + size2 + size3, NULL, GL_STATIC_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER,     0,             size1, array1);
+    glBufferSubData(GL_ARRAY_BUFFER, size1,             size2, array2);
+    glBufferSubData(GL_ARRAY_BUFFER, size1 + size2,     size3, array3);
+
+    glGenBuffers(1, ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, size0, indices, GL_STATIC_DRAW);
+
+    // Set up vertex attribute pointers for array1
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)(intptr_t) 0);
+    glEnableVertexAttribArray(0);
+
+    // Set up vertex attribute pointers for array2
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)(intptr_t) size1);
+    glEnableVertexAttribArray(1);
+
+    // Set up vertex attribute pointers for array3
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), (void*)(intptr_t) size1 + size2);
+    glEnableVertexAttribArray(2);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
