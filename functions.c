94a737f5581dd728cab2819803fc3e1e202e239c
Ramon Wong <rcnwong78@gmail.com>
2023-09-28 22:18:30 -0300

    streamed line

--- functions.c
+++ functions.c
@@ -17,20 +17,6 @@ GLuint	bMatLoc[6];
 
 GLFWwindow * wnd;
 
-void Draw3DSGrid(){
-	glColor3ub(0, 255, 0);
-	// Draw a 1x1 grid along the X and Z axis'
-	int i;
-	
-	for(i = -50; i <= 50; i += 1){
-		glBegin(GL_LINES);
-			glVertex3f(-50, -1,  i);
-			glVertex3f( 50, -1,  i);
-			glVertex3f(  i, -1, -50);
-			glVertex3f(  i, -1,  50);
-		glEnd();
-	}
-}
 
 
 
@@ -38,10 +24,8 @@ void Draw3DSGrid(){
 void Main_Loop(void){
 	double old_time = glfwGetTime();
 
-
 	GLfloat		Proj_Matrix[16];
 	GLfloat		View_Matrix[16];
-
 
 	MLoadIdentity(Proj_Matrix);
 	MLoadIdentity(View_Matrix); 
@@ -62,55 +46,29 @@ void Main_Loop(void){
 	while(!glfwWindowShouldClose(wnd)){
 		double current_time = glfwGetTime();
 		double delta_rotate = (current_time - old_time) * rotations_per_tick * 360;
-		
-
 
 		if(glfwGetKey( wnd, GLFW_KEY_ESCAPE) == GLFW_PRESS){
 			glfwSetWindowShouldClose( wnd, 1);
 		}
-				
+
 		rotate_z = 0.1 * delta_rotate;
  
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
 		
-		// glUseProgram(GLSL_Program[1]);
-		// bMatLoc[0]	= glGetUniformLocation( GLSL_Program[1], "uView_Matrix");
-		// glUniformMatrix4fv( bMatLoc[0], 1, GL_FALSE, View_Matrix);
-		
-		// bMatLoc[1]	= glGetUniformLocation( GLSL_Program[1], "uProj_Matrix");
-		// glUniformMatrix4fv( bMatLoc[1], 1, GL_FALSE, Proj_Matrix);		
-		
-		// bMatLoc[2]	= glGetUniformLocation( GLSL_Program[1], "uProjView");
-		// glUniformMatrix4fv( bMatLoc[2], 1, GL_FALSE, ProjView);			
-		// Draw3DSGrid();	
-		
-		
 		glUseProgram( GLSL_Program[0]);
 		
-		uMatLoc[0]	= glGetUniformLocation( GLSL_Program[0], "RED");
-		glUniform1f( uMatLoc[0], 1.0f);
-		
-		uMatLoc[1]	= glGetUniformLocation( GLSL_Program[0], "PI");
-		glUniform1f( uMatLoc[1], PI);
-				
-		uMatLoc[2]	= glGetUniformLocation( GLSL_Program[0], "rotate_z");
-		glUniform1f( uMatLoc[2], rotate_z);		
-				
-		uMatLoc[3]	= glGetUniformLocation( GLSL_Program[0], "uView_Matrix");
-		glUniformMatrix4fv( uMatLoc[3], 1, GL_FALSE, View_Matrix);
-		
-		uMatLoc[4]	= glGetUniformLocation( GLSL_Program[0], "uProj_Matrix");
-		glUniformMatrix4fv( uMatLoc[4], 1, GL_FALSE, Proj_Matrix);		
-		
-		uMatLoc[5]	= glGetUniformLocation( GLSL_Program[0], "uProjView");
-		glUniformMatrix4fv( uMatLoc[5], 1, GL_FALSE, ProjView);		
-		
+		glUniform1f( glGetUniformLocation( GLSL_Program[0], "RED"),				1.0f);
+		glUniform1f( glGetUniformLocation( GLSL_Program[0], "PI"),				PI);
+		glUniform1f( glGetUniformLocation( GLSL_Program[0], "rotate_z"),		rotate_z);
+		glUniformMatrix4fv( glGetUniformLocation( GLSL_Program[0], "uView_Matrix"),		1, GL_FALSE, View_Matrix);
+		glUniformMatrix4fv( glGetUniformLocation( GLSL_Program[0], "uProj_Matrix"), 	1, GL_FALSE, Proj_Matrix);
+		glUniformMatrix4fv( glGetUniformLocation( GLSL_Program[0], "uProjView"), 		1, GL_FALSE, ProjView);
 		
 		Draw();
 				
 		glfwSwapBuffers(wnd);
-			glfwPollEvents();
+		glfwPollEvents();
 	}
 }
 
