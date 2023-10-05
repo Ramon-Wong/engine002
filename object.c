484726f929513ff557e0032fe5806e4e72e1ea01
Ramon Wong <rcnwong78@gmail.com>
2023-10-05 14:12:48 -0300

    implemented texcoords

--- object.c
+++ object.c
@@ -50,7 +50,7 @@ GLubyte grid_indices[]  = { 0, 1, 2, 3};
                             //  0     1     2       3     4     5       6     7     8      9     10    11  
 // GLfloat vertices[]	= {  1.1f, 1.1f, 0.0f,	-1.1f, 1.1f, 0.0f,	-1.1f,-1.1f, 0.0f,	1.1f,-1.1f, 0.0f};
 // GLfloat Colors[]	= {  1.0f, 1.0f, 1.0f,	 1.0f, 1.0f, 1.0f,	 1.0f, 1.0f, 1.0f,	1.0f, 1.0f, 1.0f};
-// GLfloat TexCoords[]	= {  0.0f, 0.0f,	     0.0f, 0.0f,	     0.0f, 0.0f,		0.0f, 0.0f};
+
 
 GLubyte _indices[]	= {  0, 1, 2, 2, 3, 0}; 
 
@@ -73,12 +73,18 @@ void _Render( RECTANGLE * Rect, float x, float y){
     Rect->vertices[1]  = y + Rect->size; Rect->vertices[4]  = y + Rect->size; Rect->vertices[7]  = y - Rect->size; Rect->vertices[10] = y - Rect->size;
     Rect->vertices[2]  = 0.0f;           Rect->vertices[5]  = 0.0f;           Rect->vertices[8]  = 0.0f;           Rect->vertices[11] = 0.0f;
 
+    GLfloat TexCoords[]	= {  0.0f, 0.0f,	0.0f, 0.0f,	     0.0f, 0.0f,	0.0f, 0.0f};
+
     glEnableClientState(GL_VERTEX_ARRAY);
-    
+    glEnableClientState(GL_COLOR_ARRAY);
+
     glVertexPointer(3, GL_FLOAT, 0, Rect->vertices);
+    glTexCoordPointer(2, GL_FLOAT, 0, TexCoords);
+
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, _indices);
 
     glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_COLOR_ARRAY);
 }
 
 
