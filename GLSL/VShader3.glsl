fab7d8cf8fb025336e0ccbe7fe158dc6b8e08246
Ramon Wong <rcnwong78@gmail.com>
2023-10-07 22:46:25 -0300

    clean up

--- GLSL/VShader3.glsl
+++ GLSL/VShader3.glsl
@@ -16,12 +16,7 @@ vec2		texCoordArray[4];
 
 void main(){
     
-    // texCoordArray[0] = vec2( 0.0, 0.0);  //coord.data[10][0];//vec2( coord.coord.data[10][2], coord.coord.data[10][3]); 
-    // texCoordArray[1] = vec2( 1.0, 0.0);  //coord.data[10][1];//vec2( coord.coord.data[10][2], coord.coord.data[10][3]);
-    // texCoordArray[2] = vec2( 1.0, 1.0);  //coord.data[10][2];//vec2( coord.coord.data[10][4], coord.coord.data[10][5]);
-    // texCoordArray[3] = vec2( 0.0, 1.0);  //coord.data[10][3];//vec2( coord.coord.data[10][6], coord.coord.data[10][7]);
-
-    int loc = 16 * 8;
+    int loc = 33 * 8;
 
     texCoordArray[0] = vec2( coord.data[loc+0], coord.data[loc+1]); 
     texCoordArray[1] = vec2( coord.data[loc+2], coord.data[loc+3]);
