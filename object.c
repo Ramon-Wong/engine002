89b98eba978f670de590340be3ab39b5a0f05cb5
Ramon Wong <rcnwong78@gmail.com>
2023-10-09 15:02:11 -0300

    function, done

--- object.c
+++ object.c
@@ -82,6 +82,19 @@ void _Render( RECTANGLE * Rect, float x, float y){
 }
 
 
+void _RenderInstances( RECTANGLE * Rect, float x, float y, int instances){
+
+    Rect->vertices[0]  = x + Rect->width;   Rect->vertices[3]  = x - Rect->width;   Rect->vertices[6]  = x - Rect->width;   Rect->vertices[9]  = x + Rect->width;
+    Rect->vertices[1]  = y + Rect->height;  Rect->vertices[4]  = y + Rect->height;  Rect->vertices[7]  = y - Rect->height;  Rect->vertices[10] = y - Rect->height;
+    Rect->vertices[2]  = 0.0f;              Rect->vertices[5]  = 0.0f;              Rect->vertices[8]  = 0.0f;              Rect->vertices[11] = 0.0f;
+
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(3, GL_FLOAT, 0, Rect->vertices);
+    glDrawElementsInstanced(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, _indices, instances);
+    glDisableClientState(GL_VERTEX_ARRAY);
+}
+
+
 void PrintString( const char * str, ...){
     va_list args;
     va_start(args, str);
