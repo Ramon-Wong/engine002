2e4adee0b6c4f1ced2d914e7054045486456c458
Ramon Wong <rcnwong78@gmail.com>
2023-10-15 15:09:24 -0300

    clean up the scene, trying to position the cube

--- functions.c
+++ functions.c
@@ -13,12 +13,6 @@ GLfloat TexCoords[]	= {  0.0f, 0.0f,	     0.0f, 0.0f,	     0.0f, 0.0f,		0.0f, 0.
 GLubyte indices[]	= {  0, 1, 2, 2, 3, 0}; 
 
 
-// GLuint GLSL_Prog[3];
-GLuint VAO[3];
-GLuint VBO[3];
-GLuint VCO[3];
-
-
 void Draw_Object( GLuint, int);
 
 void CheckGLError(){
@@ -42,18 +36,6 @@ void CS_DrawLine( GLfloat x1, GLfloat y1, GLfloat z1, GLfloat x2, GLfloat y2, GL
 
 void Shutdown(int return_code){
 	
-	glDeleteBuffers(1, &VAO[2]);
-	glDeleteBuffers(1, &VAO[1]);
-	glDeleteVertexArrays(1, &VAO[0]);
-
-	glDeleteBuffers(1, &VBO[2]);
-	glDeleteBuffers(1, &VBO[1]);
-	glDeleteVertexArrays(1, &VBO[0]);
-
-	glDeleteBuffers(1, &VCO[2]);
-	glDeleteBuffers(1, &VCO[1]);
-	glDeleteVertexArrays(1, &VCO[0]);
-
 	glfwTerminate();
 	exit(return_code);
 }
@@ -66,20 +48,18 @@ void Main_Loop(void){
 
 	GLFWwindow * wnd = glfwGetCurrentContext();
 
-	double old_time = glfwGetTime();
+	// double old_time = glfwGetTime();
 
 	CAMERA				Camera;
 
 	GLSL_PROGRAM		Prog01;			// Landscape
 	GLSL_PROGRAM		Prog02;			// Objects
-	GLSL_PROGRAM		Prog03;			// using Orthographics
 
 	RECTANGLE			Rect;
 
 	Camera_Init(&Camera);
 	GLSLProg_Init(&Prog01);
 	GLSLProg_Init(&Prog02);
-	GLSLProg_Init(&Prog03);
 	Rectangle_Init(&Rect, 1.5f, 1.60f, 0.8f);	
 
 	float Pose[] = {  0.0f,  0.0f,  6.0f};
@@ -94,20 +74,13 @@ void Main_Loop(void){
 
 	Prog01.Init( &Prog01, "GLSL/VShader1.glsl", "GLSL/FShader1.glsl");
 	Prog02.Init( &Prog02, "GLSL/VShader2.glsl", "GLSL/FShader2.glsl");
-	Prog03.Init( &Prog03, "GLSL/VShader3.glsl", "GLSL/FShader3.glsl");
-	Prog03.LoadTexture( &Prog03, "data/skin2.tga", "tSampler", 0);											// Location 0 = gl_Texture0
-	// Prog03.CreateColorMapFBO( &Prog03, 800, 600);
-	// Prog03.CreateDepthMapFBO( &Prog03, 800, 600);
 
 	int lock = 0;
 	float point[] = { 0.0, 0.0, 0.0};
 
 	while(!glfwWindowShouldClose(wnd)){
 
-		double current_time = glfwGetTime();
-		double delta_rotate = (current_time - old_time) * rotations_per_tick * 360;
-
-		rotate_z = 0.1 * delta_rotate;
+		rotate_z = 45.0;
  
 		if( lock == 0){
 			int v = 0;	
@@ -115,15 +88,6 @@ void Main_Loop(void){
 				v += Camera.PointinPlane( &Camera, i, point);
 			}
 
-			// printf("\n");
-			// printf( "\n Point in Right	Plane: %i",	Camera.PointinPlane( &Camera, RIGHT,	point));
-			// printf( "\n Point in Left	Plane: %i",	Camera.PointinPlane( &Camera, LEFT,		point));
-			// printf( "\n Point in Back	Plane: %i",	Camera.PointinPlane( &Camera, BACK,		point));
-			// printf( "\n Point in Front	Plane: %i",	Camera.PointinPlane( &Camera, FRONT,	point));
-			// printf( "\n Point in Top	Plane: %i",	Camera.PointinPlane( &Camera, TOP,		point));    
-			// printf( "\n Point in Bottom Plane: %i",	Camera.PointinPlane( &Camera, BOTTOM,	point));
-			// printf( "\n Total Value: %i", v );
-			// printf("\n");
 			lock = 1;
 		}
 
@@ -177,10 +141,10 @@ void Main_Loop(void){
 		glEnableClientState(GL_VERTEX_ARRAY);									// Enable Vertex Arrays
 		
 		Prog02.SetUniform3f( &Prog02, "RGB", 		1.0f, 1.0f, 1.0f);
-		Prog02.gMatrixTranslation( &Prog02, 0.0, 0.0, 0.0);
-		Prog02.gMatrixRotation( &Prog02, rotate_z, 0.0, 0.0, 1.0);
-		Prog02.gMatrixRotation( &Prog02, rotate_z, 0.0, 1.0, 0.0);
-		Prog02.gMatrixRotation( &Prog02, rotate_z, 1.0, 0.0, 0.0);
+		Prog02.gMatrixTranslation( &Prog02, 0.0f, -0.5f, 0.0f);
+		Prog02.gMatrixRotation( &Prog02, rotate_z, 0.0f, 0.0f, 1.0f);
+		Prog02.gMatrixRotation( &Prog02, rotate_z, 0.0f, 1.0f, 0.0f);
+		Prog02.gMatrixRotation( &Prog02, rotate_z, 1.0f, 0.0f, 0.0f);
 		Prog02.gPopMatrix( &Prog02, "ModelMatrix");
 
 		glVertexPointer(3, GL_FLOAT, 0, box_vertices);
@@ -188,27 +152,13 @@ void Main_Loop(void){
 		glDisableClientState(GL_VERTEX_ARRAY); 									// disable Vertex Arrays
 
 		Prog02.DisableProgram(&Prog02);
-		// Prog03.DisableBufferObj(&Prog03);
-
-		Prog03.EnableProgram(&Prog03);
-		Camera.oProjView( &Camera, Prog03.GetProgram(&Prog03), "uProjView");	// need seperate camera system!
-
-		Prog03.EnableTexture(&Prog03, GL_TEXTURE0);
-		Rect.Render(&Rect, 1.7f, 1.0f);
-
-		Prog03.DisableTexture(&Prog03);
-		Prog03.DisableProgram(&Prog03);
-
-		
 
 		glfwSwapBuffers(wnd);
 		glfwPollEvents();
 		CheckGLError();
 	}
-
 	// clean stuff that is out of the loop.
 	Prog01.Release( &Prog01);
 	Prog02.Release( &Prog02);
-	Prog03.Release( &Prog03);
 }
 
