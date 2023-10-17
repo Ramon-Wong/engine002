46d2ba4b8977ab795d86d8582e7b48e0fbf387dd
Ramon Wong <rcnwong78@gmail.com>
2023-10-17 15:45:31 -0300

    kinda working!

--- GLSL/FShader2.glsl
+++ GLSL/FShader2.glsl
@@ -1,7 +1,23 @@
 #version 400
 
+uniform sampler2D tSampler;
 
+in vec4 FragPos;
+in vec4 ShadowCoord;
 
 void main(){
-    gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0);
+
+    float fDepth        = FragPos.z / FragPos.w;
+    float shadowDepth   = texture(tSampler, ShadowCoord.xy).r;
+
+    shadowDepth = shadowDepth - 0.5;
+
+
+    if (fDepth > shadowDepth) {
+        gl_FragColor = vec4( 0.0, 0.0, 0.0, 1.0);
+    } else {
+        gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0);
+    }    
+
+    // gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0);
 }
--- GLSL/VShader2.glsl
+++ GLSL/VShader2.glsl
@@ -11,9 +11,13 @@ in vec3     iArray1;                // Input vertex position
 in vec3     iArray2;                // Input Colors
 in vec2     iArray3;                // texcoords
 
-
+out vec4	FragPos;
+out vec4	ShadowCoord;
 
 void main(){
 
-	gl_Position =  uProjView * ModelMatrix * vec4( iArray1, 1.0 );
+	FragPos		=	ModelMatrix * vec4( iArray1, 1.0 );
+	ShadowCoord =	uProjView * FragPos;
+
+	gl_Position =	uProjView * FragPos;
 }
