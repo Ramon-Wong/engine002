5eac34d42fa116c0ffe3ef7fc1f5f8e4dfe096b9
Ramon Wong <rcnwong78@gmail.com>
2023-10-23 12:46:52 -0300

    stash

--- functions.c
+++ functions.c
@@ -27,31 +27,31 @@ void Shutdown(int return_code){
 
 void Main_Loop(void){
 
-	ShaderSetup();
+	// ShaderSetup();
 	glEnable(GL_DEPTH_TEST);
 
 	GLFWwindow * wnd = glfwGetCurrentContext();
 
-	GLSLProg_Init(&Prog01);
-	Camera_Init(&Camera);
+	// GLSLProg_Init(&Prog01);
+	// Camera_Init(&Camera);
 
 	// double old_time = glfwGetTime();
 	
-	float Pose[] = {  0.0f,  0.0f,  6.0f};
-	float View[] = {  0.0f,  0.0f, 12.0f}; 
-	float Upvx[] = {  0.0f,  1.0f,  0.0f};
+	// float Pose[] = {  0.0f,  0.0f,  6.0f};
+	// float View[] = {  0.0f,  0.0f, 12.0f}; 
+	// float Upvx[] = {  0.0f,  1.0f,  0.0f};
 	
-	float aspect_ratio = ((float)600) / 800;
+	// float aspect_ratio = ((float)600) / 800;
 
-	Camera.SetProjection( &Camera, 0.5f, -0.5f, -0.5f * aspect_ratio, 0.5f * aspect_ratio, 1.0f, 100.0f);	// NEW SHIT
-	Camera.SetCamera( &Camera, Pose, View, Upvx);
-	Prog01.Init( &Prog01, "GLSL/VShader.glsl", "GLSL/FShader.glsl");
+	// Camera.SetProjection( &Camera, 0.5f, -0.5f, -0.5f * aspect_ratio, 0.5f * aspect_ratio, 1.0f, 100.0f);	// NEW SHIT
+	// Camera.SetCamera( &Camera, Pose, View, Upvx);
+	// Prog01.Init( &Prog01, "GLSL/VShader.glsl", "GLSL/FShader.glsl");
 
 	while(!glfwWindowShouldClose(wnd)){
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		if(glfwGetKey( wnd, GLFW_KEY_ESCAPE) == GLFW_PRESS){	glfwSetWindowShouldClose( wnd, 1);}
-		Prog01.EnableProgram(&Prog01);
+		// Prog01.EnableProgram(&Prog01);
 
 
 
--- functions.h
+++ functions.h
@@ -8,6 +8,7 @@
 #include <GLFW/glfw3.h>
 #include <GL/glu.h> 
 #include <math.h>
+#include "objects.h"
 
 
 void				Shutdown(int);
